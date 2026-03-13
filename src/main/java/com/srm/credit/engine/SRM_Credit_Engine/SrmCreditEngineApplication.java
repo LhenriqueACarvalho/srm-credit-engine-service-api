f193e4d997bc7c6ca5a16b7000c1205e1ae969bf
LhenriqueACarvalho <lhenriqueacarvalho@gmail.com>
2026-03-13 12:52:08 -0300

    fist commit

---
+++ src/main/java/com/srm/credit/engine/SRM_Credit_Engine/SrmCreditEngineApplication.java
@@ -0,0 +1,13 @@
+package com.srm.credit.engine.SRM_Credit_Engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmCreditEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrmCreditEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srm/credit/engine/SRM_Credit_Engine/SrmCreditEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srm.credit.engine.SRM_Credit_Engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrmCreditEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
